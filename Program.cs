7faaeccce56fa750bc483ec11479a9f71a3d74fd
Caetano Barreto <caetabr@gmail.com>
2024-01-03 16:08:05 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+using Microsoft.Win32.SafeHandles;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+//criando IPhone
+Console.WriteLine("Smartphone Iphone:");
+
+        Iphone iphone = new Iphone("123456789", "iPhone X", "IMEI123", 64);
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+
+        iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Nokia:");
+        Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI456", 16);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+
+        nokia.InstalarAplicativo("PLants Vs Zombies");
